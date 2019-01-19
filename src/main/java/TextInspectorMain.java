8d4a745b6a5387d64570ec550113d740a1c00e7c
Peiki <peikimo@hotmail.it>
2019-01-19 02:19:04 +0100

    Search of all files

--- src/main/java/TextInspectorMain.java
+++ src/main/java/TextInspectorMain.java
@@ -1,5 +1,49 @@
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextInspectorMain {
     public static void main(String[] args) {
+        File directory;
+        List<String> allFiles=new ArrayList<String>();
+        directory=selectDir();
 
+        searchAllFiles(directory,allFiles);
+        for (int i = 0; i < allFiles.size(); i++) {
+            System.out.println(allFiles.get(i));
+        }
+
+        directory.getParentFile().mkdirs();
+        //directory.createNewFile();
+
+        System.out.println(allFiles.size());
+    }
+
+    private static File selectDir(){
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Seleziona la cartella contenente i file");
+        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            return chooser.getSelectedFile();
+        } else {
+            JOptionPane.showMessageDialog(null,"Errore, riprova.");
+            selectDir();
+        }
+        return null;
+    }
+
+    private static void searchAllFiles(File dir,List<String> allFiles){
+        File[] files=dir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                searchAllFiles(files[i],allFiles);
+            }
+            else{
+                allFiles.add(files[i].getPath());
+                System.out.println(files[i]);
+            }
+        }
     }
 }
